f1027ca149714642bf661eff6d984757740191f0
braveforwo <1663098442@qq.com>
2020-11-13 16:42:43 +0800

    第六次提交

--- src/test/java/com/example/demo/mapper/UserMapperTest.java
+++ src/test/java/com/example/demo/mapper/UserMapperTest.java
@@ -1,18 +1,11 @@
 package com.example.demo.mapper;
 
 import com.example.demo.domain.User;
-import com.example.demo.service.UserService;
-import com.example.demo.service.impl.UserServiceImpl;
-import com.example.demo.test.JacocoCoverage;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import tk.mybatis.spring.annotation.MapperScan;
-
-import static org.junit.Assert.*;
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
 public class UserMapperTest {
@@ -25,5 +18,5 @@ public class UserMapperTest {
          user.setUsername("1");
          user.setPassword("2");
          userMapper.insert(user);
-            }
+     }
 }
